e0dfad9f22357d16865f7a9f3d6690d84308c805
Douglas Lima <douglas07a@gmail.com>
2023-06-07 14:18:48 -0300

    section footer concluida

--- src/componentes/Rodape/index.jsx
+++ src/componentes/Rodape/index.jsx
@@ -3,29 +3,29 @@ import './style.css'
 
 const Rodape = () => (
     <footer>
-        <div>
+        <div className="conteudo-footer">
         <img src="./img/logo.png"/>
 
-        <div>
-            <h2>ENDEREÇO</h2>
+        <div className="texto-endereco">
+            <h3>ENDEREÇO</h3>
             <p>Av.Bernadino de Campos, 98 São Paulo, SP 12345-678</p>
         </div>
 
-        <div>
-            <h2>CONTATO</h2>
+        <div className="texto-contato">
+            <h3>CONTATO</h3>
             <p><a href="mailto:info@meusite.com">info@meusite.com</a></p>
             <p><a href="tel:+551134567890">(11) 3456-7890</a></p>
         </div>
 
-        <div>
-            <h2>HORÁRIOS</h2>
+        <div className="texto-horario">
+            <h3>HORÁRIOS</h3>
             <p>ABERTO TODOS OS DIAS</p>
             <p>10:00 - 22:00</p>
         </div>
 
         </div>
 
-        <div>
+        <div className="texto-rodape">
             <p>Gelateria. Orgulhosamente desenvolvido por "Douglas Lima"</p>
         </div>
     </footer>
